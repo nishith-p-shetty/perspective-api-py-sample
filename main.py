ae2aa5b0bf3b6e674c4032809dd13ed785a51a2f
Nishith P Shetty <69454628+nishith-p-shetty@users.noreply.github.com>
2023-01-13 02:19:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from googleapiclient import discovery
 import json
+import os
 
 API_KEY = 'YOUR_API_KEY'
 
@@ -40,5 +41,17 @@ analyze_request = {
 }
 
 response = client.comments().analyze( body = analyze_request ).execute()
-//print( json.dumps( response, indent = 3 ) )
-print( json.dumps( response["attributeScores"], indent = 3 ) )
+
+#print( json.dumps( response, indent = 3 ) )
+#print( json.dumps( response["attributeScores"], indent = 3 ) )
+
+params = ['TOXICITY', 'SEVERE_TOXICITY', 'IDENTITY_ATTACK', 'INSULT', 'PROFANITY', 'THREAT', 'TOXICITY_EXPERIMENTAL', 'SEVERE_TOXICITY_EXPERIMENTAL', 'IDENTITY_ATTACK_EXPERIMENTAL', 'INSULT_EXPERIMENTAL', 'PROFANITY_EXPERIMENTAL', 'THREAT_EXPERIMENTAL', 'SEXUALLY_EXPLICIT', 'FLIRTATION', 'ATTACK_ON_AUTHOR', 'ATTACK_ON_COMMENTER', 'INCOHERENT', 'INFLAMMATORY', 'LIKELY_TO_REJECT', 'OBSCENE', 'SPAM', 'UNSUBSTANTIAL']
+
+parsed = json.dumps( response["attributeScores"])
+parsed = json.loads(parsed)
+
+for i in params:
+    tmp = parsed[i]
+    tmp = tmp['summaryScore']
+    #print(i , " : " , tmp['value'])
+    print( '{:28s} : {:4}'.format(i, tmp['value']) )
